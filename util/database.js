970f728ca221c32dbabb33b5cad19f5032c666c0
bharat kumar <bharatkumar18002@gmail.com>
2022-10-27 22:32:27 +0530

    database connection issue fix

--- util/database.js
+++ util/database.js
@@ -2,8 +2,13 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(`${process.env.mysqldb}`,`${process.env.username}`,`${process.env.password}`,{
     dialect: 'mysql', 
-    host: 'localhost',
-    logging: false
+    host: '127.0.0.1',
+    logging: false,
+    dialectOptions: {
+        socketPath: '/var/run/mysqld/mysqld.sock',
+        port: '3306'
+ 
+      }
 
 });
 
