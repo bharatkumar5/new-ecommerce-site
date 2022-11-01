472bf530b3380495016f9bcd9be30fa5ad2ca9e6
bharat kumar <bharatkumar18002@gmail.com>
2022-11-01 16:58:58 +0530

    online database setup

--- util/database.js
+++ util/database.js
@@ -1,16 +1,31 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize(`${process.env.mysqldb}`,`${process.env.username}`,`${process.env.password}`,{
+// const sequelize = new Sequelize(`${process.env.mysqldb}`,`${process.env.username}`,`${process.env.password}`,{
+//     dialect: 'mysql', 
+//     host: 'containers-us-west-102.railway.app',
+//     logging: false
+    
+
+// });
+
+
+const sequelize = new Sequelize('railway','root','GExWVLHKBZ36yD7xKIBv',{
     dialect: 'mysql', 
-    host: '127.0.0.1',
-    logging: false,
-    dialectOptions: {
-        socketPath: '/var/run/mysqld/mysqld.sock',
-        port: '3306'
- 
-      }
+    host: 'containers-us-west-102.railway.app',
+    port: 6532,
+    logging: false
+    
 
 });
 
+
+
+
+
+
     module.exports = sequelize;
     
+
+    // mysql -hcontainers-us-west-69.railway.app -uroot -p******** --port 7641 --protocol=TCP railway
+
+    // mysql://root:ORKl3ZqDJL3Og2mFNutt@containers-us-west-69.railway.app:7641/railway
